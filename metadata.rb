b189d4edd818368c4183e4fad00c408b294ceca2
sohiljain.cs91@gmail.com <Madadj_12>
2016-08-01 17:55:43 -0700

    slkjf

--- metadata.rb
+++
@@ -1,7 +0,0 @@
-name 'mydocker'
-maintainer 'The Authors'
-maintainer_email 'you@example.com'
-license 'all_rights'
-description 'Installs/Configures mydocker'
-long_description 'Installs/Configures mydocker'
-version '0.1.0'
--- mydocker/metadata.rb
+++ mydocker/metadata.rb
@@ -5,3 +5,5 @@ license 'all_rights'
 description 'Installs/Configures mydocker'
 long_description 'Installs/Configures mydocker'
 version '0.1.0'
+
+depends 'docker', '~> 2.0'
