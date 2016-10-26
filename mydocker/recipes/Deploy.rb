2888c537800f6de61f8926f65083b23070caf7b7
Sohil Jain <sohiljain.cs91@gmail.com>
2016-10-26 17:00:47 -0500

    Create Deploy.rb

---
+++ mydocker/recipes/Deploy.rb
@@ -0,0 +1,22 @@
+#
+# Cookbook Name:: mydocker
+# Recipe:: default
+#
+# Copyright (c) 2016 The Authors, All Rights Reserved.
+docker_service 'default' do
+  action [:create, :start]
+end
+
+docker_image 'image_1' do
+  tag 'latest'
+  source '/home/sohil/docker-sbt-sample/'
+  action :build
+end
+
+# Run container exposing ports
+docker_container 'my_image' do
+  repo 'image_1'
+  tag 'latest'
+  port '80:80'
+  action :run
+end
