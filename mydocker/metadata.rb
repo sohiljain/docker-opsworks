a3d5df51a4df6130c001b0c4209c29a118c4e17c
Sohil Jain <sohiljain.cs91@gmail.com>
2017-01-26 18:28:06 -0800

    Update metadata.rb

--- mydocker/metadata.rb
+++ mydocker/metadata.rb
@@ -7,11 +7,3 @@ long_description 'Installs/Configures mydocker'
 version '0.1.0'
 
 depends 'docker', '~> 2.0'
-
-# name        "deployfile"
-# description "Deploy File From S3"
-# maintainer  "Dilbert"
-# license     "Apache 2.0"
-# version     "1.0.0"
-
-depends "aws"
